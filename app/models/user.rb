ac3d4dd36fd13a9f62461492c31441c80fce7590
Kobayashi-T01 <t-kobayashi@MacBook-Pro.local>
2020-11-04 18:15:30 +0900

    コメントアウト箇所削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,14 +17,4 @@ class User < ApplicationRecord
   end
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
-  # validates :nickname, presence: true
-  # validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  # validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  # validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  # validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  # validates :birth_day, presence: true
-  # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-
-  # has_many :items
-  # has_many :buys
 end
