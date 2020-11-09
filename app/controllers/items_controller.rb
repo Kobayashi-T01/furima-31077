af1bc17e92e7a3df96fd8003e5b3de5332d6cd5a
Kobayashi-T01 <t-kobayashi@MacBook-Pro.local>
2020-11-09 22:42:37 +0900

    商品情報登録画面遷移・ビューファイル修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
   end
-
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explain, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
-  belongs_to :delivery_fee
   belongs_to :area
   belongs_to :day
-
+  belongs_to :delivery_fee
 
   belongs_to :user
   has_one :buy
