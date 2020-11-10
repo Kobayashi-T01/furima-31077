0d63c7f8170874afb9ab900a0f83394fb7e1aa04
Kobayashi-T01 <t-kobayashi@MacBook-Pro.local>
2020-11-10 19:34:18 +0900

    java手数料・利益計算表示実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,14 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  const priceTax = document.getElementById("add-tax-price");
-  const priceProfit = document.getElementById("profit");
-  console.log(priceProfit);
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const priceTax = document.getElementById("add-tax-price");
+    priceTax.innerHTML = Math.floor(inputValue * 0.1);
+    const priceTaxResult = (inputValue * 0.1);
+
+    const priceProfit = document.getElementById("profit");
+    priceProfit.innerHTML = Math.floor(inputValue - priceTaxResult);
+  })
 });
