b39ef7190cb6728ebd08c36e03cfb4459a46e51e
Kobayashi-T01 <t-kobayashi@MacBook-Pro.local>
2020-11-10 18:33:54 +0900

    javaファイル作成・HTML要素取得

---
+++ app/javascript/item_price.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const priceTax = document.getElementById("add-tax-price");
+  const priceProfit = document.getElementById("profit");
+  console.log(priceProfit);
+});
