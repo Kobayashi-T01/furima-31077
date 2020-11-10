e020aa5c11efb4252669f7dc892bfdfbf2022bfa
Kobayashi-T01 <t-kobayashi@MacBook-Pro.local>
2020-11-10 20:14:57 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,10 +37,22 @@ describe Item do
         expect(@item.errors.full_messages).to include("Category is not a number")
       end
 
+      it 'カテゴリーが1の時' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it '商品の状態が空の時' do
         @item.status_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Status is not a number")
+      end
+
+      it '商品の状態が1の時' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it '配送料の負担が空の時' do
@@ -49,16 +61,34 @@ describe Item do
         expect(@item.errors.full_messages).to include("Delivery fee is not a number")
       end
 
+      it '配送料の負担が1の時' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
+
       it '発送元の地域が空の時' do
         @item.area_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Area is not a number")
       end
 
+      it '発送元の地域が1の時' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+
       it '発送までの日数が空の時' do
         @item.day_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Day is not a number")
+      end
+
+      it '発送までの日数が1の時' do
+        @item.day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
       end
 
       it '販売価格が空の時' do
